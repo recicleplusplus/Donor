21bcb587e2587f5bdd254b16a690ec0cb485038b
ReciclePlus <166835215+recicleplusplus@users.noreply.github.com>
2024-06-09 23:01:33 -0300

    Update index.jsx

--- src/screens/home/index.jsx
+++ src/screens/home/index.jsx
@@ -28,7 +28,7 @@ export function Home({}) {
   const navigation = useNavigation();
   const firestore = getFirestore(firebaseApp);
   const {donorState, donorDispach} = useContext(DonorContext)
-  const basedImage                       = require("../../../assets/images/profile.webp");
+  const basedImage                       = require("../../../assets/images/profile2.webp");
   const [image, setImage]                = useState(basedImage);
   const [tarefas, setTarefas]            = useState([]);
   const quantidadeTarefas = tarefas.length;
